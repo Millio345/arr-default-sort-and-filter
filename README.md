8bd7907f0068c69548b98bcdcca472bcf3c88034
Gerto <gert.joos@gmail.com>
2024-03-14 13:37:44 +0100

    Add support for lidarr,readarr,sonarr and whisparr.

--- README.md
+++ README.md
@@ -1,21 +1,25 @@
 # arr-default-sort-and-filter
-Userscript to set default sort and filter options for Radarr
+Userscript to set default sort and filter options for Lidarr / Radarr/ Readarr / Sonarr / Whisparr
 
 ## Usage
-1. Install the userscript using your preferred Userscript manager (FirMonkey/Tampermonkey/Greasemonkey/Violentmonkey/...)
-2. Load your Radarr installation in your website
-3. Open your userscript manager's menu and under script commands for "Radarr Default Sort And Filter", click "Configure Radarr default sort and filter"
+1. Install the userscript using your preferred Userscript manager (FireMonkey/Tampermonkey/Greasemonkey/Violentmonkey/...)
+2. Load your *arr installation in your website
+3. Open your userscript manager's menu and under script commands for "*arr Default Sort And Filter", click "Configure Radarr default sort and filter"
 4. Configure the default filter and sort and click "Save" and "Close". For icon columns, enter the text that apears when you move your mouse over the icon. A valid sort example for an english installation is "Custom Format Score".
 5. Open a movie and do an interactive search, the sort and filter should be applied
 
-## Known Issues
-When an 'Interactive Search' table is open, the configuration values become impossible to edit. Reload the page and configure the script before loading the interactive search. 
+## Known Issues / Limitations
+- When an 'Interactive Search' table is open, the configuration values become impossible to edit. Reload the page and configure the script before loading the interactive search.
+- The script only works for interactive searches which open in a separate Modal window. For example in 'Readarr', when on a book page, the search is on the page instead of in a separate modal window; Go to the author page and initiate the search from there.
+- Only one configuration set per *arr type is possible: For example if you manage two Radarr sites, you cannot set different filter/sort options for each. 
+
+Feel free to open an issue / PR on GitHub if one of these provide a problem in how you use the script.
 
 ## Troubleshooting
 Open your developer console using F12.
-If the script is correctly loaded and finds a Radarr installation, you should see the message "Radarr installation found. Watching for interactive search table".
+If the script is correctly loaded and finds an *arr installation, you should see the message like "Radarr installation found. Watching for interactive search table".
 
 If the script cannot find the sort column or filter it will log a warning and log a list of valid filters / columns.
 
 ## Feature Requests
-Currently the script only supports Radarr and only Interactive Search, but if you'd like support for other *arrs or other sortable tables in Radarr, feel free to open an issue to request this.
+If you'd like additional features, feel free to open an issue to request this.
--- radarr-default-sort-and-filter.js
+++ radarr-default-sort-and-filter.js
@@ -1,27 +1,28 @@
 // ==UserScript==
-// @name        Radarr default sort & filter
+// @name        *arr default sort & filter
 // @namespace   https://github.com/Millio345/arr-default-sort-and-filter
-/* Since Radarr's installation url is different for every user we need to match all links; Script checks for a 'Radarr' metadata tag before actually doing anything */
+/* Since *arr's installation url is different for every user we need to match all links; Script checks for a 'Radarr' metadata tag before actually doing anything */
 // @include http://*
 // @include https://*
 // @require     https://openuserjs.org/src/libs/sizzle/GM_config.js
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM_registerMenuCommand
-// @version     1.1
+// @version     1.2
 // @author      Millio
-// @description Sets default sort and filter options for Radarr's interactive search
+// @description Sets default sort and filter options for Radarr/Sonarr/...'s interactive search
 // @license      MIT
 // ==/UserScript==
 
-/* This script allows you to define default sorting and filter for your interactive movie searches for Radarr
+/* This script allows you to define default sorting and filter for your interactive searches for the arr websites.
 As of version 1.1 you no longer need to make any changes to this file.
 USAGE:
-After installing the extension, visit your Radarr installation and click the 'Configure Radarr default sort & filter' button which should show up in your Userscript extension's menu on your browser
+After installing the extension, visit your Radarr/Sonarr/... installation and click the 'Configure *arr default sort & filter' button which should show up in your Userscript extension's menu on your browser
 
 Problems? Suggestions? Improvements? GitHub at https://github.com/Millio345/arr-default-sort-and-filter
 
 CHANGELOG:
+1.2 - Add support for Lidarr, Sonarr, Readarr & Whisparr
 1.1 - Move all configuration to UI to prevent users having to override this file with every update. Allow sorting by icon columns by entering the label text you see when moving your mouse over the element (span title)
 1.0 - Initial release, basic sort & filter functionality
 */
@@ -98,7 +99,7 @@ CHANGELOG:
             return
         }
 
-        // Only click filter button once per movie until page is changed; Otherwise the user would not be able to override the filter since the script would always take over
+        // Only click filter button once after opening a modal; Otherwise the user would not be able to override the filter since the script would always take over
         if(filterButtonClicked){
             return
         }
@@ -128,6 +129,17 @@ CHANGELOG:
         console.log("Valid filters:"+filterList.join(','));
     }
 
+    // Observe the body element so we know when modal is opened / closed
+    function bodyClassChanged(mutationsList, observer){
+        mutationsList.forEach(mutation => {
+            if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
+                // Modal was closed; Set filterButtonClicked to false so we can reclick it next time the modal is opened
+                if(!mutation.target.className.includes("Modal-modalOpen")) {
+                    filterButtonClicked = false;
+                }
+            }
+        });
+    }
     // This function is called by the mutationObserver when additional page content is added. Check if it contains the search table or the filter 'scroller'
     function handleMutations(mutationsList, observer) {
         for (let mutation of mutationsList) {
@@ -139,7 +151,7 @@ CHANGELOG:
                         if (node.nodeType === 1 && typeof node.className === 'string')
                         {
                             // Was a tableContainer in a modal added?
-                            if(node.className.includes('Table-tableContainer-')&&node.parentElement.parentElement.className.startsWith('ModalBody')) {
+                            if(node.className.includes('Table-tableContainer-')&&node.closest("[class^='ModalBody']")) {
                                 clickColumnAndFilter(node);
                                 return;
                             }
@@ -154,52 +166,85 @@ CHANGELOG:
         }
     }
 
-    // Check current page url to see if we're on a movie page or not ; returns true/false
-    function onMoviePage(){
-        // movie links end in /movie/NUMBER
-        let pattern = /\/movie\/\d+$/;
+    // Check current page url to see if we're on page with interactive search button or not
+    function onPageWithInteractiveSearchButton(arr_type) {
+        let pattern;
 
-        // Return true / false depending on whether we're on a movie page
+        // Depending on which arr site we're on we check the url
+        if(arr_type==="Lidarr") {
+            // artist/albums links end in /(artist|album)/artist-or-album-name
+            pattern = /\/(artist|album)\/[\w-]+$/;
+        }
+        else if(arr_type==="Radarr") {
+            // movie links end in /movie/NUMBER
+            pattern = /\/movie\/\d+$/;
+        }
+        else if(arr_type==="Readarr") {
+
+            // author links end in /author/NUMBER
+            // Known limitation: The book page does not offer an interactive search modal and is currently not supported; Go to the author page and search from there.
+            pattern = /\/author\/\d+$/;
+        }
+        else if(arr_type==="Sonarr") {
+
+            // series links end in /series/series-name
+            pattern = /\/series\/[\w-]+$/;
+        }
+        else if(arr_type==="Whisparr") {
+            // site links end in /site/sitename
+            pattern = /\/site\/[\w-]+$/;
+        }
+
+        // Return true / false depending on whether we're on a media page
         return pattern.test(window.location.href);
     }
 
+
     // When a movie page is loaded, start mutation observer and set filter clicked to false
-    function movieLoaded(){
+    function interactiveSearchPageLoaded(){
         filterButtonClicked = false
         observer.observe(document.documentElement, { childList: true, subtree: true });
     }
 
     // To prevent unintended consequences on other tables/buttons, stop observer when another page is loaded
-    function nonMoviePageLoaded(){
+    function nonInteractiveSearchPageLoaded(){
         observer.disconnect()
     }
-    function loadScript()
+    function loadScript(arr_type)
     {
+        // Set Variable prefix to the arr type (f.e. SonarrInteractiveSearchFilterName
+        let prefix = arr_type;
+
+        // To prevent breaking changes to previous installations when this script only supported Radarr, Radarr variables don't have a prefix.
+        if(prefix==="Radarr"){
+            prefix="";
+        }
         // Setup GM Config, which provides a nice UI to set the options
         gmc = new GM_config(
             {
-                'id': 'RadarrDefaultSortAndFilter',
-                "css": `#RadarrDefaultSortAndFilter {background: #2a2a2a;}
-                #RadarrDefaultSortAndFilter .field_label {color: #fff;}
-                #RadarrDefaultSortAndFilter .config_header {color: #fff; padding-bottom: 10px;}
-                #RadarrDefaultSortAndFilter .reset {color: #f00; text-align: center;}
-                #RadarrDefaultSortAndFilter .section_header {color: #fff; text-align: left; margin-top: 15px;margin-bottom: 5px; padding:5px;}
-                #RadarrDefaultSortAndFilter .section_desc {background: #2a2a2a; color: #fff; text-align: left; border:none;}
-                #RadarrDefaultSortAndFilter .config_var {text-align: left;}
-                #RadarrDefaultSortAndFilter input { display: block; margin-top:5px; margin-bottom: 20px}
-                #RadarrDefaultSortAndFilter .reset_holder {display: none;}`,
-                'title': 'Radarr Default Sort and Filter', // Panel Title
+                // We need unique id for each site, otherwise all non-specified values will be cleared when saving.
+                'id': prefix+'ArrDefaultSortAndFilter',
+                "css": `#`+prefix+`ArrDefaultSortAndFilter {background: #2a2a2a;}
+                #`+prefix+`ArrDefaultSortAndFilter .field_label {color: #fff;}
+                #`+prefix+`ArrDefaultSortAndFilter .config_header {color: #fff; padding-bottom: 10px;}
+                #`+prefix+`ArrDefaultSortAndFilter .reset {color: #f00; text-align: center;}
+                #`+prefix+`ArrDefaultSortAndFilter .section_header {color: #fff; text-align: left; margin-top: 15px;margin-bottom: 5px; padding:5px;}
+                #`+prefix+`ArrDefaultSortAndFilter .section_desc {background: #2a2a2a; color: #fff; text-align: left; border:none;}
+                #`+prefix+`ArrDefaultSortAndFilter .config_var {text-align: left;}
+                #`+prefix+`ArrDefaultSortAndFilter input { display: block; margin-top:5px; margin-bottom: 20px}
+                #`+prefix+`ArrDefaultSortAndFilter .reset_holder {display: none;}`,
+                'title': arr_type+' Default Sort and Filter', // Panel Title
                 'fields': // Fields object
                     {
-                        'InteractiveSearchFilterName': // This is the id of the field
+                        [prefix+'InteractiveSearchFilterName']: // This is the id of the field
                             {
                                 'label': 'Name of default filter to apply (leave empty to disable)', // Appears next to field
                                 'section': [GM_config.create('Automatic Filter'), 'Automatically applies a filter when loading interactive search table'],
                                 'type': 'text', // Makes this setting a text field
-                                'title': 'Enter the name of the filter exactly as you see it in Radarr',
+                                'title': 'Enter the name of the filter exactly as you see it in '+arr_type,
                                 'default': '' // Default value if user doesn't change it
                             },
-                        'InteractiveSearchSortColumn': // This is the id of the field
+                        [prefix+'InteractiveSearchSortColumn']: // This is the id of the field
                             {
                                 'label': 'Default interactive search column to sort by (leave empty to disable)', // Appears next to field
                                 'section': [GM_config.create('Automatic Sort'), 'If you want the script to automatically apply a filter when loading interactive search, enter the exact name of the filter here'],
@@ -207,7 +252,7 @@ CHANGELOG:
                                 'type': 'text', // Makes this setting a text field
                                 'default': '' // Default value if user doesn't change it
                             },
-                        'InteractiveSearchSortAscending': // This is the id of the field
+                        [prefix+'InteractiveSearchSortAscending']: // This is the id of the field
                             {
                                 'label': 'Sort Ascending? (Check to sort A-Z / 0 - 9)', // Appears next to field
                                 'type': 'checkbox', // Makes this setting a text field
@@ -217,27 +262,27 @@ CHANGELOG:
                 'events': {
                     'init': () => {
                         // initialization complete, load values
-                        filterName = gmc.get('InteractiveSearchFilterName');
-                        sortColumn = gmc.get('InteractiveSearchSortColumn');
-                        sortAscending = gmc.get('InteractiveSearchSortAscending');
+                        filterName = gmc.get([prefix+'InteractiveSearchFilterName']);
+                        sortColumn = gmc.get([prefix+'InteractiveSearchSortColumn']);
+                        sortAscending = gmc.get([prefix+'InteractiveSearchSortAscending']);
                     },
                     'save': function () { // runs after values are saved
                         // settings may have been changed, reload values
-                        filterName = gmc.get('InteractiveSearchFilterName');
-                        sortColumn = gmc.get('InteractiveSearchSortColumn');
-                        sortAscending = gmc.get('InteractiveSearchSortAscending');
+                        filterName = gmc.get([prefix+'InteractiveSearchFilterName']);
+                        sortColumn = gmc.get([prefix+'InteractiveSearchSortColumn']);
+                        sortAscending = gmc.get([prefix+'InteractiveSearchSortAscending']);
                     }
                 }
             });
 
         // Add configuration menu item to extension
-        GM_registerMenuCommand("Configure Radarr default sort & filter", show_config, "c");
+        GM_registerMenuCommand("Configure "+arr_type+" default sort & filter", show_config, "c");
 
         // Initial page load - Are we on a movie page?
-        if(onMoviePage())
-            movieLoaded()
+        if(onPageWithInteractiveSearchButton(arr_type))
+            interactiveSearchPageLoaded()
 
-        // Code below to detect page change without reload radarr uses - https://stackoverflow.com/questions/6390341/how-to-detect-if-url-has-changed-after-hash-in-javascript
+        // Code below to detect page change without reload *arr uses - https://stackoverflow.com/questions/6390341/how-to-detect-if-url-has-changed-after-hash-in-javascript
         let oldPushState = history.pushState;
         history.pushState = function pushState() {
             let ret = oldPushState.apply(this, arguments);
@@ -261,11 +306,11 @@ CHANGELOG:
         // locationchange event is fired when url is changed thanks to code above
         window.addEventListener('locationchange', function () {
             //Are we on a movie page? Call appropriate function
-            if(onMoviePage())
-                movieLoaded()
+            if(onPageWithInteractiveSearchButton(arr_type))
+                interactiveSearchPageLoaded()
             // When on another page, disable the mutation observer
             else
-                nonMoviePageLoaded()
+                nonInteractiveSearchPageLoaded()
         });
 
     }
@@ -276,17 +321,25 @@ CHANGELOG:
     }
 
     /* END FUNCTIONS */
+    // Following code is ran on every single tab so should be kept as light as possible
+    // Support all sites listed under Media Automation on https://wiki.servarr.com/
+    var supportedSites = ["Lidarr","Radarr","Readarr","Sonarr","Whisparr"];
 
-
-    // First check if we're on a Radarr site (site with metadata description 'Radarr')
+    // Check if we're on a supported site (site with metadata description 'Radarr'/'Sonarr'/...)
     let description = document.querySelector('meta[name="description"]');
-    if(description && description.content==="Radarr"){
-        console.log("Radarr installation found. Watching for interactive search table")
+
+    if(description && supportedSites.includes(description.content)){
+        let arr_type=description.content
+        console.log(arr_type+" installation found. Watching for interactive search table.")
         // Define some variables for global use through the script
-        // Create a new MutationObserver since Radarr dynamically adds elements after pageload
+        // Create a new MutationObserver since *arr sites dynamically add elements after pageload
         var observer = new MutationObserver(handleMutations);
 
-        // Radarr uses ajax to change pages; We only want to click the filter button once per movie (otherwise the filter button is completely blocked from user interaction)
+        // Observe the body element for changes to the class (= modal opened / closed)
+        var body_observer = new MutationObserver(bodyClassChanged);
+        body_observer.observe(document.querySelector('body'), { attributes: true, attributeFilter: ['class'], attributeOldValue: true });
+
+        // *arr sites uses ajax to change pages; We only want to click the filter button once per movie (otherwise the filter button is completely blocked from user interaction)
         var filterButtonClicked = false;
 
         // Variable to store gm config
@@ -298,7 +351,6 @@ CHANGELOG:
         var sortAscending;
 
         // Load the script functionality
-        loadScript();
-
+        loadScript(arr_type);
     }
 })();
